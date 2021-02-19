583714b62a09f99b740f75474d156ac7e70c6de5
VaSeBa <badasin.valentin@yandex.ru>
2021-02-19 07:55:37 +0300

    commit 19.02

---
+++ app/src/main/java/ru/vaseba/geoquiz/Question.java
@@ -0,0 +1,24 @@
+package ru.vaseba.geoquiz;
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
+//61
